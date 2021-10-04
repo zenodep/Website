854beb9e6995508403841480a24f9e076bcc8837
zenodep <zenodep@gmail.com>
2021-10-04 22:05:34 +0200

    Replaces login info with env variables

--- Website/settings.py
+++ Website/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zuurkool92@gmail.com'
-EMAIL_HOST_PASSWORD = 'continua'
+EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
